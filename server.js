823413538fd6b485c70db0df9a8bd21d7d6a452f
Hariienesh1901 <hariienesh.cool@gmail.com>
2021-12-09 18:21:58 +0800

    Initial Commit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
 const express = require("express");
@@ -11,21 +11,19 @@ server.get("/", (req, res) => {
     res.send("Moderator Bot");
 });
 
-server.all("*", (req, res) => {
-    server.all("/", (req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.send(`Moderator Bot \nPath: ${req.ori}`)
-        next();
-    });
-})
+server.all("*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.send(`Moderator Bot \nPath: ${req.originalUrl}`);
+    next();
+});
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 const keepAlive = () => {
     server.listen(port, () => {
-        console.log(`Server started on port ${port}`)
-    })
-}
+        console.log(`Server started on port ${port}`);
+    });
+};
 
 module.exports = keepAlive;
